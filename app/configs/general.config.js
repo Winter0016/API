cfec65ccfc5f6d969928315a854fbdfa23c98bac
Winter0016 <chauquangphuc2604.2604@gmail.com>
2023-08-17 10:32:59 +0700

    ok

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -2,9 +2,9 @@ module.exports = {
     DBConnectors: {
         host: process.env.DB_HOST || "db4free.net",
         port: process.env.DB_PORT || 3306,
-        username: process.env.DB_USER || "audiobook",
-        password: process.env.DB_PASSWORD || "thanhbinh88",
-        database: process.env.DB_NAME || "audiobook",
+        username: process.env.DB_USER || "chauquangphuc",
+        password: process.env.DB_PASSWORD || "monkeyking2604",
+        database: process.env.DB_NAME || "mydatabase2004",
         dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
