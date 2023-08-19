4cc3f6643fd6507a5cefc2adda2ac5802bbe7249
Winter0016 <chauquangphuc2604.2604@gmail.com>
2023-08-19 08:44:03 +0700

    no message

--- app/services/category.service.js
+++ app/services/category.service.js
@@ -1,14 +1,74 @@
 const rest = require('../utils/restware.util');
 const category = require('../models/category.model');
 module.exports = {
-    getAll: function (req, res) {
-        const out = { title: 'categories', id: 'all'};
-        return rest.sendSuccessOne(res, out, 200);
+    getAll: function(req, res) {
+        const query = req.query || '';
+        try {
+            const where = {};
+            let page = 1;
+            let perPage = 10;
+            const sort = [];
+            const offset = perPage * (page - 1);
+
+            category.findAndCountAll({
+                where: where,
+                limit: perPage,
+                offset: offset,
+                order: sort,
+                raw: true,
+            })
+                .then((data) => {
+                    const pages = Math.ceil(data.count / perPage);
+                    const output = {
+                        data: data.rows,
+                        pages: {
+                            current: page,
+                            prev: page - 1,
+                            hasPrev: false,
+                            next: (page + 1) > pages ? 0 : (page + 1),
+                            hasNext: false,
+                            total: pages,
+                        },
+                        items: {
+                            begin: ((page * perPage) - perPage) + 1,
+                            end: page * perPage,
+                            total: data.count,
+                        },
+                    };
+                    output.pages.hasNext = (output.pages.next !== 0);
+                    output.pages.hasPrev = (output.pages.prev !== 0);
+                    return rest.sendSuccessMany(res, output, 200);
+                }).catch(function(error) {
+                return rest.sendError(res, 1, 'get_list_category_fail', 400, error);
+            });
+        } catch (error) {
+            return rest.sendError(res, 1, 'get_list_category_fail', 400, error);
+        }
     },
 
+
     getOne: function (req, res) {
-        let id = req.params.id || '';
-        res.send('categories: ' + id);
+        const id = req.params.id || '';
+        try {
+            const attributes = ['id', 'title', 'created_at', 'updated_at', 'created_by', 'updated_by'];
+
+            const where = {id: id};
+
+            book.findOne({
+                where: where,
+                attributes: attributes,
+                raw: true,
+            }).then((result)=>{
+                'use strict';
+                if (result) {
+                    return rest.sendSuccessOne(res, result, 200);
+                } else {
+                    return rest.sendError(res, 1, 'unavailable_category', 400);
+                }
+            });
+        } catch (error) {
+            return rest.sendError(res, 400, 'get_category_fail', 400, error);
+        }
     },
 
     createOne: function (req, res) {
